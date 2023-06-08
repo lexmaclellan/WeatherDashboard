9ab0bd4b568c594718d0aab44edf587de87eb102
lexrayne <lexrayne@outlook.com>
2023-06-08 00:04:54 -0300

    Added location name translation to coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,40 @@
-var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=6da9f270f26dfadb040379c56c4a5070";
+var searchUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Fredericton&limit=5&appid=6da9f270f26dfadb040379c56c4a5070";
 
-function getApi(requestUrl) {
-  fetch(requestUrl)
+function getCoordinates(searchUrl) {
+    fetch(searchUrl)
     .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-      }
-      else {
-        console.log('Error: ' + response.statusText);
-      }
-  })
-  .catch(function (error) {
-    console.log('Unable to connect to OpenWeather');
-  });
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+                getWeather(data.items[0].lat, data.items[0].lon);
+            });
+        }
+        else {
+            console.log("Error: " + response.statusText);
+        }
+    })
+    .catch(function (error) {
+        console.log("Unable to connect to OpenWeather");
+    });
 }
 
-getApi(requestUrl);
+var getWeather = function(lat, lon) {
+    var weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=6da9f270f26dfadb040379c56c4a5070`;
+    
+    fetch(weatherUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                })
+            }
+            else {
+                console.log("Error: " + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            console.log("Unable to connect to OpenWeather");
+        });
+};
+
+getCoordinates(weatherUrl);
