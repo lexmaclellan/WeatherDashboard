d2f74dd18c8f7537b2c893612636d4d8bfb2a118
lexrayne <lexrayne@outlook.com>
2023-06-11 22:26:41 -0300

    Wrote a clearer function for five day forecasts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,46 @@
+var apiKey = "6da9f270f26dfadb040379c56c4a5070";
 var formEl = document.querySelector("#searchForm");
 var cityInputEl = document.querySelector("#citySearch");
 var searchButtonEl = document.querySelector("#searchButton");
 var weatherDetailsEl = document.querySelector("#weatherDetails");
 
 var getWeather = function(city) {
-    var weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&limit=5&appid=6da9f270f26dfadb040379c56c4a5070`;
+    var weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&limit=5&appid=${apiKey}`;
     fetch(weatherUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
                     console.log(data);
-                    
-                    //for (var i = 0; i < data.length; i++) {
-                        var weatherDetailsCard = document.createElement("div");
-                        weatherDetailsCard.textContent = dayjs.unix(data.dt).format("MMM D, YYYY");
-                        weatherDetailsCard.textContent += ", " + toCelsius(data.main.temp) + " ° C";
-                        weatherDetailsEl.appendChild(weatherDetailsCard);
-                    //}
+                    var weatherDetailsCard = document.createElement("div");
+                    var weatherDetailsDate = document.createElement("h2");
+                    var weatherDetailsTemp = document.createElement("span");
+
+                    weatherDetailsDate.textContent = dayjs.unix(data.dt).format("MMM D, YYYY");
+                    weatherDetailsTemp.textContent = toCelsius(data.main.temp) + " ° C";
+
+                    weatherDetailsCard.appendChild(weatherDetailsDate);
+                    weatherDetailsCard.appendChild(weatherDetailsTemp);
+                    weatherDetailsEl.appendChild(weatherDetailsCard);
+
+                    fiveDayForecast(data.coord.lat, data.coord.lon);
+                })
+            }
+            else {
+                console.log("Error: " + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            console.log("Unable to connect to OpenWeather");
+        })
+}
+
+var fiveDayForecast = function(lat, lon) {
+    var forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    fetch(forecastUrl)
+        .then (function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
                 })
             }
             else {
